f5d2e963046cc76f18222dcc8dedde9dac0b56d6
pengqian089 <pengqian089@gmail.com>
2022-11-11 18:04:03 +0800

    add ace editor

--- dashboard/editor.js
+++ dashboard/editor.js
@@ -1,6 +1,16 @@
-function initEditor(){
-    new Cherry({
-        id: 'editor-demo',
-        value: '# welcome to cherry editor!',
-    });
+
+
+function initEditor(id,value) {
+    let editor = ace.edit(id);
+    editor.setTheme("ace/theme/tomorrow_night");
+    editor.session.setMode("ace/mode/markdown");
+    editor.setValue(value, -1);
+    editor.session.setUseWrapMode(true);
+    editor.session.setTabSize(4);
+    editor.session.setUseSoftTabs(true);
+    editor.setShowPrintMargin(false);
+    // editor.session.on('change', function(delta) {
+    //     let markdown = document.getElementById("hide-markdown");
+    //     markdown.value = editor.getValue();
+    // });
 }
