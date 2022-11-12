c821586eb447d37a0c4362effbdb402d30aa2b56
pengqian089 <pengqian089@gmail.com>
2022-11-12 18:48:41 +0800

    ace editor

--- dashboard/editor.js
+++ dashboard/editor.js
@@ -1,16 +1,34 @@
+var aceEditor = {
+    editor : null,
+    initEditor: function (id, value) {
+        aceEditor.editor = ace.edit(id);
+        aceEditor.editor.setTheme("ace/theme/tomorrow_night");
+        aceEditor.editor.session.setMode("ace/mode/markdown");
+        aceEditor.editor.setValue(value, -1);
+        aceEditor.editor.session.setUseWrapMode(true);
+        aceEditor.editor.session.setTabSize(4);
+        aceEditor.editor.session.setUseSoftTabs(true);
+        aceEditor.editor.setShowPrintMargin(false);
+        // editor.session.on('change', function(delta) {
+        //     let markdown = document.getElementById("hide-markdown");
+        //     markdown.value = editor.getValue();
+        // });
+    },
+    initHtmlEditor:function(id,value){
+        aceEditor.editor = ace.edit(id);
+        aceEditor.editor.setTheme("ace/theme/tomorrow_night");
+        aceEditor.editor.session.setMode("ace/mode/html");
+        aceEditor.editor.setValue(value, -1);
+        aceEditor.editor.session.setUseWrapMode(true);
+        aceEditor.editor.session.setTabSize(4);
+        aceEditor.editor.session.setUseSoftTabs(true);
+        aceEditor.editor.setShowPrintMargin(false);
+    },
+    getValue : function (){
+        return aceEditor.editor?.getValue() ?? "";
+    },
+    appendValue:function(value){
+        aceEditor.editor?.insert(value);
+    }
+};
 
-
-function initEditor(id,value) {
-    let editor = ace.edit(id);
-    editor.setTheme("ace/theme/tomorrow_night");
-    editor.session.setMode("ace/mode/markdown");
-    editor.setValue(value, -1);
-    editor.session.setUseWrapMode(true);
-    editor.session.setTabSize(4);
-    editor.session.setUseSoftTabs(true);
-    editor.setShowPrintMargin(false);
-    // editor.session.on('change', function(delta) {
-    //     let markdown = document.getElementById("hide-markdown");
-    //     markdown.value = editor.getValue();
-    // });
-}
